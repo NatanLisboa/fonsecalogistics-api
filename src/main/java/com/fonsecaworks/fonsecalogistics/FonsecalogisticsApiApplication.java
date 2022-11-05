71b949a34da77326aaf07879facdd24224c2ac99
NatanLisboa <natanflisboa1@gmail.com>
2022-11-05 16:19:00 -0300

    Add basic Spring project structure

---
+++ src/main/java/com/fonsecaworks/fonsecalogistics/FonsecalogisticsApiApplication.java
@@ -0,0 +1,13 @@
+package com.fonsecaworks.fonsecalogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FonsecalogisticsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FonsecalogisticsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fonsecaworks/fonsecalogistics/FonsecalogisticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fonsecaworks.fonsecalogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FonsecalogisticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
