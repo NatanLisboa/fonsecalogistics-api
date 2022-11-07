930b82921e04c0bf775daf0dee10cc246544c9df
NatanLisboa <natanflisboa1@gmail.com>
2022-11-07 03:16:36 -0300

    Add findCustomerById method in CustomerController

--- src/main/java/com/fonsecaworks/fonsecalogistics/api/controller/CustomerController.java
+++ src/main/java/com/fonsecaworks/fonsecalogistics/api/controller/CustomerController.java
@@ -2,7 +2,10 @@ package com.fonsecaworks.fonsecalogistics.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fonsecaworks.fonsecalogistics.domain.model.Customer;
@@ -12,13 +15,21 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/customers")
 public class CustomerController {
 	
 	private CustomerRepository customerRepository;
 	
-	@GetMapping("/customers")
+	@GetMapping
 	public List<Customer> findAllCustomers() {
 		return customerRepository.findAll();		
+	}
+	
+	@GetMapping("/{customerId}")
+	public ResponseEntity<Customer> findCustomerById(@PathVariable Long customerId) {
+		return customerRepository.findById(customerId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());		
 	}
 	
 }
